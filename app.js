8e0518a2bcab3fbd8a0ebb38c8edbe0e4b8b2822
zschuah <zschuah@hotmail.com>
2022-11-27 02:00:13 +0800

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      randomCorgi: "https://source.unsplash.com/300x300/?corgi",
+      isCorgiShown: false,
+      isCorgiLoading: false,
+    };
+  },
+  methods: {
+    changeCorgi() {
+      if (!this.isCorgiLoading) {
+        this.isCorgiShown = !this.isCorgiShown;
+
+        if (!this.isCorgiShown) {
+          this.isCorgiLoading = true;
+          console.log("changing...");
+
+          fetch("https://source.unsplash.com/300x300/?corgi").then((res) => {
+            console.log(res.url);
+            this.randomCorgi = res.url;
+            this.isCorgiLoading = false;
+          });
+        }
+      }
+    },
+  },
+  mounted() {
+    console.log("App is mounted!");
+  },
+});
+
+app.mount("#vue-mount");
